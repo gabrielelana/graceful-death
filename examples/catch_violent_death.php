064ef53459aff63cb09b49f13f630d255a433ecc
gabriele.lana <gabriele.lana@cleancode.it>
2014-04-11 11:34:09 +0200

    Better example

--- examples/catch_violent_death.php
+++ examples/catch_violent_death.php
@@ -2,12 +2,16 @@
 
 require __DIR__ . "/../vendor/autoload.php";
 
+// The output will be: Yes, I can ;-)
+
 GracefulDeath::around(function() {
     try {
+        // Avoid to print the error in order to have clean output, don't try this at home :-)
         error_reporting(E_ALL ^ E_ERROR);
-        // Instance an unknown class cause a fatal error
+        // Creating an instance of an unknown class will cause a fatal error
         new UnknownClass();
     } catch(Exception $e) {
+        // A fatal error is uncatchable
         echo "You cannot catch this, AHAHAHAHA!!!\n";
     }
 })
